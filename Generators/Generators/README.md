f3a9e5696e4d0ace8cce6b1e76f97a87bff88c85
Bryan Tiete Tanoue <bryan_tanoue@hotmail.com>
2021-01-27 12:59:22 -0300

    Exemplos e explicação de generators

---
+++ Generators/Generators/README.md
@@ -0,0 +1,2 @@
+Aulas de introdução a generators
+
---
+++ Generators/Generators/generator.js
@@ -0,0 +1,30 @@
+//criando a regra para ser iterável
+const obj = {
+    values: [1,2,3,4],
+    *[Symbol.iterator]() {
+        for (var i = 0; i < this.values.length; i++) {
+            yield this.values[i]; //pausa a função, mandando esse valor para fora e depois volta
+        }
+    }
+};
+/*
+function* hello(){
+    console.log('Hello');
+    yield 1;
+
+    console.log('Form');
+    const value = yield 2;
+
+    console.log(value);
+}
+
+const it3 = hello();
+
+console.log(it3.next());
+console.log(it3.next());
+console.log(it3.next('Outside'));
+*/
+
+for (let value of obj) {
+    console.log(value);
+}
